4b4e7eedd25534bc806d21379c9621608439649e
Mirza Abdullah Baig <abaig7921@gmail.com>
2025-01-08 12:07:42 +0500

    some changes

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import { v4 as uuidv4 } from "uuid"; // Unique id generator
 // import { faker } from "@faker-js/faker"; // fake username generator
 
 const app = express();
-const port = process.env.port || 3000;
+const port = 3000;
 
-app.use(cors({ origin: ["http://localhost:5173", "http://localhost:3000"] }))
+// app.use(cors({ origin: "*" })); // Allow all origins for now
+app.use(cors({ origin: ["http://localhost:5173"] }))
 app.use(express.json());
 
 const todos = [];
